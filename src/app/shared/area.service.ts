b2094ff597b0c2e431bc750e7587ad2052ba086c
ccampos <ccampos@exact.com.pe>
2018-10-31 12:37:02 -0500

    Historias Terminadas

--- src/app/shared/area.service.ts
+++ src/app/shared/area.service.ts
@@ -3,24 +3,24 @@ import { Area } from './../../model/area.model';
 import { Injectable } from "@angular/core";
 import { RequesterService } from "./requester.service";
 import { AppSettings } from "./app.settings";
-import { Observable } from "rxjs";
+import { Observable, Subject } from "rxjs";
 
 
 @Injectable()
 export class AreaService {
 
-    constructor(
-        private requesterService: RequesterService
-    ){}
-
     REQUEST_URL = AppSettings.API_ENDPOINT + AppSettings.AREA_URL;
 
+    constructor(private requester: RequesterService){
+    }
+
+
     listarAreasAll(): Observable<Area[]>{
-        return this.requesterService.get(this.REQUEST_URL, {});
+        return this.requester.get<Area[]>(this.REQUEST_URL, {});
     }
 
     public actualizarPlazoDistribucionPermitido(areaId: number, plazoDistribucionPermitido: PlazoDistribucion): Observable<PlazoDistribucion> {
-        return this.requesterService.put<PlazoDistribucion>(this.REQUEST_URL + areaId.toString() + "/plazosdistribucion", plazoDistribucionPermitido, {});
+        return this.requester.put<PlazoDistribucion>(this.REQUEST_URL + areaId.toString() + "/plazosdistribucion", plazoDistribucionPermitido, {});
     }
 
 }
