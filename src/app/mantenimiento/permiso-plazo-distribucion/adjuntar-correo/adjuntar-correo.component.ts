e5604faa598218e9fb466f60ebcb2b37e60e4e56
ccampos <ccampos@exact.com.pe>
2019-06-03 12:30:54 -0500

    correo area + alineacion de guias activas

--- src/app/mantenimiento/permiso-plazo-distribucion/adjuntar-correo/adjuntar-correo.component.ts
+++ src/app/mantenimiento/permiso-plazo-distribucion/adjuntar-correo/adjuntar-correo.component.ts
@@ -8,6 +8,8 @@ import { BuzonService } from 'src/app/shared/buzon.service';
 import { UtilsService } from 'src/app/shared/utils.service';
 import { Buzon } from 'src/model/buzon.model';
 import { PlazoDistribucion } from 'src/model/plazodistribucion.model';
+import { AreaService } from 'src/app/shared/area.service';
+import { Area } from 'src/model/area.model';
 
 @Component({
   selector: 'app-adjuntar-correo',
@@ -20,13 +22,16 @@ export class AdjuntarCorreoComponent implements OnInit {
     public bsModalRef: BsModalRef,
     public utilsService: UtilsService,
     private notifier: NotifierService,
-    private buzonService: BuzonService
+    private buzonService: BuzonService,
+    private areaService: AreaService
   ) { }
 
   buzon: Buzon;
+  area: Area;
   plazoDistribucion: PlazoDistribucion;
   correoForm: FormGroup;
   archivoAdjunto: File;
+  valor: string;
 
   @Output() correoAdjuntadoEvent = new EventEmitter<File>();
 
@@ -46,20 +51,29 @@ export class AdjuntarCorreoComponent implements OnInit {
 
   onSubmit(archivoAdjunto: File) {
     if (this.utilsService.isUndefinedOrNull(archivoAdjunto)) {
-      this.notifier.notify("success", "Seleccione el archivo excel a subir");
+      this.notifier.notify("success", "Seleccione el archivo del correo");
       return;
     }
-    console.log("BUZON: " + this.buzon);
-    this.buzonService.actualizarPlazoDistribucionPermitido(this.buzon.id, this.plazoDistribucion, archivoAdjunto).subscribe(
-      plazoDistribucion => {
-        this.notifier.notify('success', 'Se ha asignado correctamente el plazo de distribución');
-        this.correoAdjuntadoEvent.emit();
-        this.bsModalRef.hide();
-      }
-    )
-    // this.correoAdjuntadoEvent.emit();
-    // this.bsModalRef.hide();
+
+    if (this.valor === "buzon"){
+      this.buzonService.actualizarPlazoDistribucionPermitido(this.buzon.id, this.plazoDistribucion, archivoAdjunto).subscribe(
+        plazoDistribucion => {
+          this.notifier.notify('success', 'Se ha asignado correctamente el plazo de distribución');
+          this.correoAdjuntadoEvent.emit();
+          this.bsModalRef.hide();
+        }
+      )
+    } else {
+      this.areaService.actualizarPlazoDistribucionPermitido(this.area.id, this.plazoDistribucion, archivoAdjunto).subscribe(
+        plazoDistribucion => {
+          this.notifier.notify('success', 'Se ha asignado correctamente el plazo de distribución');
+          this.correoAdjuntadoEvent.emit();
+          this.bsModalRef.hide();
+        }
+      )
+    }
   }
+
 
 
 }
