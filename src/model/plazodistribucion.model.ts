8e1ca086ed32709fc64b7f10a14664ac95b2b87d
ccampos <ccampos@exact.com.pe>
2019-07-11 12:18:15 -0500

    validacion de devolucion y model plazo

--- src/model/plazodistribucion.model.ts
+++ src/model/plazodistribucion.model.ts
@@ -3,15 +3,14 @@ import { Ambito } from "./ambito.model";
 import { Region } from "./region.model";
 
 export class PlazoDistribucion {
-    // constructor(public id: number, public nombre:string, public tipoPlazoDistribucion: TipoPlazoDistribucion, public tiempoEnvio: number , public activo: boolean){}
+    
     constructor(
-        public id: number, 
+        public id: number,
         public nombre:string, 
         public tiempoEnvio: number,
-        public tipoPlazoDistribucion: TipoPlazoDistribucion, 
-        public rutaAutorizacion: string, 
+        public tipoPlazoDistribucion: TipoPlazoDistribucion,
+        public rutaAutorizacion: string,
         public activo: boolean,
-        public ambitos: Ambito[] = [],
         public regiones: Region[] = []
         ){}
 
